d3342655d1b24092e4516e37fe9e7d048c2ce19f
GauthierParvillers <gauthier.parvillers@viacesi.fr>
2018-05-31 11:33:18 +0200

    add v1.0 of element

---
+++ Mobile/Element.java
@@ -0,0 +1,52 @@
+/*
+ * Element class of the Lorann project
+ * 
+ * @author : Gauthier Parvillers
+ * @version : 1.0
+ * 
+ * */
+
+
+package model;
+
+import java.awt.image.BufferedImage;
+import java.util.Observable;
+
+
+public abstract class Element implements IElement {
+	
+	protected BufferedImage image;
+	protected boolean permeability;
+	
+	public BufferedImage getImage() {
+		return image;
+		/*
+		 * 
+		 * This will permit to access the image and use it here
+		 * 
+		 * 
+		 * */
+	}
+
+	public boolean isPermeability() {
+		return permeability;
+		
+		/*
+		 * will permit to get the permeability of the "surface"
+		 * true or false
+		 * */
+	}
+
+
+	
+	public Element(String path, boolean permability) {
+		
+	}
+	
+	protected static BufferedImage loadSprite(String path) {
+		
+	}
+	
+	
+
+}
