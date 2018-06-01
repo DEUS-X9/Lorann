3e201381fe78d424723aa7530e30b5fbdc92a4db
GauthierParvillers <gauthier.parvillers@viacesi.fr>
2018-06-01 15:36:27 +0200

    original dao + class model v1.2

---
+++ Mobile/Model.java
@@ -0,0 +1,153 @@
+/**
+ * Model class, will permit to instantiate the map
+ */
+
+package model;
+
+import java.awt.*;
+import java.sql.SQLException;
+import java.util.Hashtable;
+import java.util.Observable;
+
+import contract.IElement;
+import contract.IModel;
+import model.mobile.*;
+import model.motionless.*;
+
+public class Model extends Observable implements IModel {
+	
+	private String map = "";
+			/**the map will be stocked here*/
+			
+	private int score = 0;
+	/**creation of the score*/
+	
+	/**instantiation of a new model*/
+	public Model() {
+		
+		
+	}
+		
+		/**association of letter with a sprite*/
+		
+		public IElement element(char c, Point pos) {
+			switch (c) {
+			
+			case '' :
+				return new Bone();
+				
+			case '' :
+				return new HorizontalBone();
+				
+			case '' :
+				return new VerticalBone();
+				
+			case '' :
+				return new EnergyBubble();
+				
+			case '' :
+				return new ClosedDoor();
+				
+			case '' :
+				return new Purse();
+				
+			case '' :
+				return new Hero(pos);
+				
+			case '' :
+				return new Sorcery(pos);
+				
+			case '' :
+				return new Monster1(pos);
+				
+			case '' :
+				return new Monster2(pos);
+				
+			case '' :
+				return new Monster3(pos);
+				
+			case '' :
+				return new Monster4(pos);
+			
+			case '' :
+				return new Score();
+				
+			case '' :
+				return new Title();
+				
+			case '' :
+				return empty();
+			}
+			
+			public Observable getObservable() {
+				return this.map;
+			}
+			
+			public String getMap() {
+				return this.map;
+				/**
+				 * get the map and @return it
+				 */
+			}
+			
+			private void setMap(final String map) {
+				this.map = map;
+				this.setChanged();
+				this.notifyObserver();
+				/**Sets the map
+				 * @param map contains the String of the map
+				 */
+			}
+			
+			public void loadMap(String key) {
+				try {
+					final DAOLoadMap daoLoadMap = new DAOLoadMap(LorannBDDConnector.getInstance().getConnection());
+					this.setMap(daoLoadMap.find(key).getMap());
+				} catch (final SQLException e) {
+					e.printStackTrace();
+				}}
+				
+				
+				/** upload of the score to the database
+				 * @param score contains the score
+				 * @nickname contains the nickname of the player
+				 */
+				
+				public void  upNameAndScore(int score, String nickname) {
+					try
+					
+					{
+						 final DAOUploadScore daoUploadScore = new DAOUploadScore(LorannBDDConnector.getInstance().getConnection());
+						 daoUploadScore.upNameAndScore(score,nickname);
+					}
+					
+					catch (final SQLException e)
+					{
+						e.printStackTrace();
+					}
+				}
+				
+				
+				
+				public String[][] getHighSore()
+				{
+					try {
+						final DAOGetHighScore daoGetHighscore = new DAOGetHighscore(LorannBDDConnector.getInstance().getConnection());
+						return (daoGetHighScore.getHighScore());
+					}
+					catch (final SQLEception e)
+					{
+						e.printStackTrace();
+					}
+					return null;
+				}
+			}
+			
+			
+			
+			
+		}
+		
+	}
+			
+}
---
+++ Mobile/motionless/HorizontalBoneTest.java
@@ -0,0 +1,14 @@
+package model.motionless;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HorizontalBoneTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
