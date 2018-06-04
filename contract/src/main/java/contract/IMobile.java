e24d0df7ae3e58073dc530f1045779ac1a7c8f37
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2018-06-04 15:47:55 +0200

    Update IMobile.java

--- contract/src/main/java/contract/IMobile.java
+++ contract/src/main/java/contract/IMobile.java
@@ -1,5 +1,27 @@
 package contract;
 
-public interface IMobile {
+import java.awt.*;
+
+public interface IMobile extends IElement {
+  
+  void setLocation(Point loc);
+
+    /**
+     * Only for mobile element. Get the position on the map before apply motion
+     * @return the Abscissa and ordinate of the mobile element
+     * @author pierre fleury
+     */
+    Point getPos();
+
+    /**
+     * @param order
+     * Use the order to move the element on the array map removes or decreases the abscissa OR the ordinate frome its actual position
+     */
+    void move(MobileOrder order);
+
+    /**
+     * @return the direction of the element
+     */
+    MobileOrder getDirection();
 
 }
