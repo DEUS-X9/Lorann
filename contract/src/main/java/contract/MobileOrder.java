b15708360dfb66f62c5433f2c816c9d5b8e5d3b1
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2018-06-04 15:53:48 +0200

    Update MobileOrder.java

--- contract/src/main/java/contract/MobileOrder.java
+++ contract/src/main/java/contract/MobileOrder.java
@@ -1,5 +1,11 @@
 package contract;
 
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public enum MobileOrder {
 	
 	Right,
