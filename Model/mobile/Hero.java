e4f03abef39b9e941f2616f0f14fb5961a881a82
Alexi <33145849+DEUS-X9@users.noreply.github.com>
2018-06-05 11:12:31 +0200

    Update Hero.java
    Additional feature for Lorann

--- Model/mobile/Hero.java
+++ Model/mobile/Hero.java
@@ -24,59 +24,90 @@ public class Hero extends Mobile implements IHero {
 	}
 	
 	
-	public void move(MobileOrder order, IElement[][] tileMap, IView view) {
-		/**
-		 * This function will permit to move the character around
-		 * 
-		 */
-		
-		switch (order) {
-		
-		case Left:
-			if(this.direction == MobileOrder.Left && this.pos.y > 0 && tileMap[this.pos.x][this.pos.y - 1].getpermeability()){
-				this.pos.setLocation(
-					this.pos.getX(),
-					this.pos.getY() - 1);
-			} else {
-                this.image = Element.loadSprite("lorann_l.png");
-            }
-            break;
-			
-			
-		case Right:
-			if(this.direction == MobileOrder.Right && this.pos.y > 0 && tileMap[this.pos.x][this.pos.y + 1].getpermeability()){
-				this.pos.setLocation(
-					this.pos.getX(),
-					this.pos.getY() + 1);
-			} else {
-                this.image = Element.loadSprite("lorann_r.png");
-            }
-            break;
-			
-			
-		case Up:
-			if(this.direction == MobileOrder.Up && this.pos.y > 0 && tileMap[this.pos.x + 1][this.pos.y].getpermeability()){
-				this.pos.setLocation(
-					this.pos.getX() + 1,
-					this.pos.getY());
-			} else {
-                this.image = Element.loadSprite("lorann_u.png");
-            }
-            break;
-			
-			
-		case Down:
-			if(this.direction == MobileOrder.Down && this.pos.y > 0 && tileMap[this.pos.x - 1][this.pos.y].getpermeability()){
-				this.pos.setLocation(
-					this.pos.getX() - 1,
-					this.pos.getY());
-			} else {
-                this.image = Element.loadSprite("lorann_d.png");
-            }
-            break;
-		
-	}
-		this.direction = order;
-		}
-
+    public void move(MobileOrder order, IElement[][] tileMap, IView view)
+    {
+        switch (order) {
+            case Left:
+                if(this.direction == MobileOrder.Left &&
+                        this.pos.y > 0 &&
+                        tileMap[this.pos.x][this.pos.y - 1].getPermeability())
+                {
+                    this.pos.setLocation(
+                            this.pos.getX(),
+                            this.pos.getY() - 1);
+                } else if(this.direction != MobileOrder.Left &&
+                        this.pos.y > 0 &&
+                        tileMap[this.pos.x][this.pos.y - 1].getPermeability()) 
+                {
+                	this.image = Element.loadSprite("lorann_l.png");
+                	 this.pos.setLocation(
+                             this.pos.getX(),
+                             this.pos.getY() - 1);
+                }
+                else{
+                    this.image = Element.loadSprite("lorann_l.png");
+                }
+                break;
+            case Right:
+                if(this.direction == MobileOrder.Right &&
+                        this.pos.y < view.getWidth() / 32 - 1 &&
+                        tileMap[this.pos.x][this.pos.y + 1].getPermeability())
+                {
+                    this.pos.setLocation(
+                            this.pos.getX(),
+                            this.pos.getY() + 1);
+                } else if(this.direction != MobileOrder.Right &&
+                        this.pos.y < view.getWidth() / 32 - 1 &&
+                        tileMap[this.pos.x][this.pos.y + 1].getPermeability()) 
+                {
+                	this.pos.setLocation(
+                            this.pos.getX(),
+                            this.pos.getY() + 1);
+                	this.image = Element.loadSprite("lorann_r.png");
+                }
+                else {
+                    this.image = Element.loadSprite("lorann_r.png");
+                }
+                break;
+            case Up:
+                if(this.direction == MobileOrder.Up &&
+                        this.pos.x > 0 &&
+                        tileMap[this.pos.x - 1][this.pos.y].getPermeability()) {
+                    this.pos.setLocation(
+                            this.pos.getX() - 1,
+                            this.pos.getY());
+                }else if(this.direction != MobileOrder.Up &&
+                        this.pos.x > 0 &&
+                        tileMap[this.pos.x - 1][this.pos.y].getPermeability()) 
+                {
+                	this.pos.setLocation(
+                            this.pos.getX() - 1,
+                            this.pos.getY());
+                	this.image = Element.loadSprite("lorann_u.png");
+                }else {
+                    this.image = Element.loadSprite("lorann_u.png");
+                }
+                break;
+            case Down:
+                if(this.direction == MobileOrder.Down &&
+                        this.pos.x < view.getHeight() / 32 - 1 &&
+                        tileMap[this.pos.x + 1][this.pos.y].getPermeability()) {
+                    this.pos.setLocation(
+                            this.pos.getX() + 1,
+                            this.pos.getY());
+                }else if(this.direction != MobileOrder.Down &&
+                        this.pos.x < view.getHeight() / 32 - 1 &&
+                        tileMap[this.pos.x + 1][this.pos.y].getPermeability()) 
+                {
+                	this.pos.setLocation(
+                             this.pos.getX() + 1,
+                             this.pos.getY());
+                	this.image = Element.loadSprite("lorann_b.png");
+                }else {
+                    this.image = Element.loadSprite("lorann_b.png");
+                }
+                break;
+        }
+        this.direction = order;
+    }
 }
