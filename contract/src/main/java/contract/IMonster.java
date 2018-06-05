d56eb91a30c1dee02add21386b50cd24febd69d4
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2018-06-05 09:18:20 +0200

    Update IMonster.java

--- contract/src/main/java/contract/IMonster.java
+++ contract/src/main/java/contract/IMonster.java
@@ -1,5 +1,14 @@
 package contract;
 
+import java.awt.*;
+
 public interface IMonster {
+  
+  /**
+     * @param heroPos for Monster 1, act as an pathfinder, an "AI" for the monster
+     * @param tileMap check elements permeability and size map
+     * @return direction (change position of the monster on the map)
+     */
+    MobileOrder getDirection(Point heroPos, IElement[][] tileMap);
 
 }
